e01546aee2669f8e8b8bbec82bd31f838fd0f1cf
UTS-zhy <2691448414@qq.com>
2020-10-01 17:49:03 +0800

    Fist Update

---
+++ main.py
@@ -0,0 +1,7 @@
+import sqlite3
+import urllib.request
+print("This is PyBlog!")
+print("Made By UTS")
+print("@underthestars-zhy/PyBlog")
+url = 'https://cdn.jsdelivr.net/gh/underthestars-zhy/PyBlog@master/pyblog.txt'
+print("当前版本：Beta-01")
