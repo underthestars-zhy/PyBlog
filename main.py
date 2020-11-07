d2de0016fdf5cd9c4fbdd6e28c62245770eff90d
Underthestars-zhy <zhuhaoyu0909@icloud.com>
2020-11-07 15:28:11 +0800

    :zap:Beta5.1 !!!

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+'''
+Made by UTS
+WebSite: https://uts.ski
+'''
 def date_celebrate():
     import datetime
     month_ = int(datetime.datetime.now().month)
@@ -5,25 +9,28 @@ def date_celebrate():
     if month_ == 10 and day_ == 1:
         return "Happy National Day, the first version of PyBlog was released today!"
     elif month_ == 10 and day_ == 31:
-        return "The Develop setting was added for the first time."
+        return "The Develop setting was added for the first time.\ntrick or treat!"
+    elif month_ == 12 and day_ == 25:
+        return "Merry Christmas!"
 
+def data_cmd():
+    import datetime
+    month_ = int(datetime.datetime.now().month)
+    day_ = int(datetime.datetime.now().day)
+    if month_ == 10 and day_ == 1:
+        return True
+    elif month_ == 10 and day_ == 31:
+        return True
+    elif month_ == 12 and day_ == 25:
+        return True
 if __name__ == '__main__':
-    '''
-    Made by UTS
-    '''
     import sqlite3
     import os
     import sys
     import time
-    try:
-        import web
-    except:
-        webpy_import = False
     # Variable forward declaration
-    develop_web = False
     develop_celebrate = False
-    develop_CLI_SSH = False
-    develop_CLI_SQL = False
+    develop_theme_version = False
     # Main program
     os.system("rm -rf __pycache__")
     os.system("rm version.txt")
@@ -48,16 +55,15 @@ if __name__ == '__main__':
         cur_log.execute('''Create table Themes(id int,name text)''')
         cur_log.execute('''Create table IndexD(id int,tf int)''')
         cur_log.execute('''Create table DevelopSet(id int,tf int)''')
+        cur_log.execute('''Create table BackUp_Pages(tittle text, path text, time text, content text, html_content text, pic text)''')
         print("现在我们需要一些注册信息")
         user_name = str(input("User_Name:"))
         user_password = str(input("User_Password:"))
         cur_log.execute('''insert into Account Values(1,'{}','{}')'''.format(user_name, user_password))
         cur_log.execute('''insert into Themes Values(1,'no themes')''')
         cur_log.execute('''insert into IndexD Values(1,0)''')
-        cur_log.execute('''insert into DevelopSet Values(1,0)''')  # Web.py
-        cur_log.execute('''insert into DevelopSet Values(2,0)''')  # celebrate
-        cur_log.execute('''insert into DevelopSet Values(3,0)''')  # Client(SSH)
-        cur_log.execute('''insert into DevelopSet Values(4,0)''')  # Client(SQL)
+        cur_log.execute('''insert into DevelopSet Values(1,0)''')  # celebrate
+        cur_log.execute('''insert into DevelopSet Values(2,0)''')  # ThemeVersion
         conn_log.commit()
         cur_log.execute('select * from Themes')
         t_name = ""
@@ -84,6 +90,13 @@ if __name__ == '__main__':
             account = 1
         else:
             print("NO")
+        # Develop Select
+        cur_log.execute('select * from DevelopSet')
+        for developset_in in cur_log.fetchall():
+            if developset_in[0] == 1 and developset_in[1] == 1:
+                develop_celebrate = True
+            elif developset_in[0] == 2 and developset_in[1] == 1:
+                develop_theme_version= True
     else:
         os.system("clear")
         print("+++++Error#0-PyBlog!+++++")
@@ -98,28 +111,38 @@ if __name__ == '__main__':
         print("Github@underthestars-zhy/PyBlog")
         print("==========================================")
         sys.exit()
+    if develop_theme_version:
+        try:
+            from theme_main import *
+            os.system("rm version_theme.txt")
+            os.system('wget ' + least_version_url())
+            file = open('version_theme.txt', 'rt')
+            version_theme = str(file.read())
+            file.close()
+            os.system("rm version_theme.txt")
+            theme_version = "当前主题版本: " + version() + " 最新主题版本: " + version_theme
+        except:
+            theme_version = "当前主题不支持/你还未安装主题"
     while True:
-        # Develop Set Select
+        # Develop Select
         conn_log = sqlite3.connect("log.db")
         cur_log = conn_log.cursor()
         cur_log.execute('select * from DevelopSet')
         for developset_in in cur_log.fetchall():
             if developset_in[0] == 1 and developset_in[1] == 1:
-                develop_web = True
-            elif developset_in[0] == 2 and developset_in[1] == 1:
                 develop_celebrate = True
-            elif developset_in[0] == 3 and developset_in[1] == 1:
-                develop_CLI_SSH = True
-            elif developset_in[0] == 4 and developset_in[1] == 1:
-                develop_CLI_SQL = True
+            elif developset_in[0] == 2 and developset_in[1] == 1:
+                develop_theme_version = True
         conn_log.close()
         # Main Program
         os.system("clear")
         print("+++++Welcome-PyBlog!+++++")
         print("+++++++Made By UTS+++++++")
         print("Github@underthestars-zhy/PyBlog")
-        if develop_celebrate:
-            print("Celebrate"+date_celebrate())
+        if develop_celebrate and data_cmd():
+            print("Celebrate: "+date_celebrate())
+        if develop_theme_version:
+            print(theme_version)
         print("==========================================")
         print("0: 退出, 1: 设置, 2: 主题, 3: 页面, 99: Develop Set")
         main_set = int(input("选择："))
@@ -257,7 +280,7 @@ if __name__ == '__main__':
                     com = main_txt()
                     print("+++++" + t_name + "设置+++++")
                     print("==========================================")
-                    print("0: Back, 1: 创建主题主文件 " + com)
+                    print("0: Back, 1: 创建主题主文件" + com)
                     theme_main_set = int(input("选择: "))
                     if theme_main_set == 0:
                         continue
@@ -380,6 +403,7 @@ if __name__ == '__main__':
                     print("==========================================")
                     print("Upload your files to the current directory")
                     about_page_name = input('File Nmae: ')
+                    about_page_pic = input('Pic URL: ')
                     try:
                         file = open(about_page_name + '.md', 'r')
                         about_txt = str(file.read())
@@ -397,13 +421,23 @@ if __name__ == '__main__':
                     from theme_write import *
                     from datetime import *
                     today = datetime.now()
-                    if page_write('about', datetime.date(today), about_txt):
+                    if page_write('about', datetime.date(today), about_txt, about_page_pic):
                         os.system('clear')
-                        print("+++++Up About Page-PyBlog!+++++")
+                        sql_1 = str(datetime.date(today))
+                        sql_2 = str(markdown.markdown(about_txt))
+                        conn_log = sqlite3.connect("log.db")
+                        cur_log = conn_log.cursor()
+                        cur_log.execute(
+                            '''insert into BackUp_Pages Values('About','about.html','{0}','{1}','{2}','{3}')'''.format(
+                                sql_1, sql_2, about_txt, about_page_pic
+                            )
+                        )
+                        conn_log.commit()
+                        conn_log.close()
+                        print("+++++Upload About Page-PyBlog!+++++")
                         print("==========================================")
                         print("Success!")
                         import time
-
                         time.sleep(2)
                         continue
                     else:
@@ -414,7 +448,73 @@ if __name__ == '__main__':
                         print("==========================================")
                         print("Please Retry")
                         import time
-
+                        time.sleep(2)
+                        continue
+                elif about_set == 2:
+                    os.system("clear")
+                    print("+++++Write About Page-PyBlog!+++++")
+                    print("==========================================")
+                    print("Please write below, if the writing is completed, please enter: WriteOK")
+                    i = True
+                    import random
+                    file_name = str(int(random.uniform(0,10)))+str(int(random.uniform(0,10)))+str(int(random.uniform(0,10)))+".md"
+                    file = open(file_name, 'a')
+                    while i:
+                        input_txt = str(input())
+                        if input_txt != "WriteOK":
+                            file.write(input_txt + "\r")
+                        else:
+                            i = False
+                    file.close()
+                    print("输入成功!")
+                    time.sleep(1)
+                    os.system('clear')
+                    about_page_pic = input('Pic URL: ')
+                    try:
+                        file = open(file_name, 'r')
+                        about_txt = str(file.read())
+                        file.close()
+                    except:
+                        os.system("clear")
+                        print("+++++Warning#1-PyBlog!+++++")
+                        print("+++++++Made By UTS+++++++")
+                        print("Github@underthestars-zhy/PyBlog")
+                        print("==========================================")
+                        print("Please Retry")
+                        import time
+                        time.sleep(2)
+                        continue
+                    os.system('''rm {0}'''.format(file_name))
+                    from theme_write import *
+                    from datetime import *
+                    today = datetime.now()
+                    if page_write('about', datetime.date(today), about_txt, about_page_pic):
+                        os.system('clear')
+                        sql_1 = str(datetime.date(today))
+                        sql_2 = str(markdown.markdown(about_txt))
+                        conn_log = sqlite3.connect("log.db")
+                        cur_log = conn_log.cursor()
+                        cur_log.execute(
+                            '''insert into BackUp_Pages Values('About','about.html','{0}','{1}','{2}','{3}')'''.format(
+                                sql_1, sql_2, about_txt, about_page_pic
+                            )
+                        )
+                        conn_log.commit()
+                        conn_log.close()
+                        print("+++++Up About Page-PyBlog!+++++")
+                        print("==========================================")
+                        print("Success!")
+                        import time
+                        time.sleep(2)
+                        continue
+                    else:
+                        os.system("clear")
+                        print("+++++Warning#0-PyBlog!+++++")
+                        print("+++++++Made By UTS+++++++")
+                        print("Github@underthestars-zhy/PyBlog")
+                        print("==========================================")
+                        print("Please Retry")
+                        import time
                         time.sleep(2)
                         continue
                 else:
@@ -451,7 +551,7 @@ if __name__ == '__main__':
                     print(">>> pip install markdown")
                     sys.exit()
                 os.system('clear')
-                print("+++++Write-PyBlog!+++++")
+                print("+++++Write Pages-PyBlog!+++++")
                 print("==========================================")
                 print("0: Back, 1: Up, 2: Write online")
                 write_set = int(input())
@@ -459,7 +559,7 @@ if __name__ == '__main__':
                     continue
                 elif write_set == 1:
                     os.system('clear')
-                    print("+++++Write-PyBlog!+++++")
+                    print("+++++Upload Page-PyBlog!+++++")
                     print("==========================================")
                     print("Upload your files to the current directory")
                     page_name = input('File Nmae: ')
@@ -469,7 +569,6 @@ if __name__ == '__main__':
                     page_txt = str(file.read())
                     file.close()
                     from theme_write import *
-
                 else:
                     os.system("clear")
                     print("+++++Error#0-PyBlog!+++++")
@@ -491,40 +590,42 @@ if __name__ == '__main__':
             print("Github@underthestars-zhy/PyBlog")
             print("==========================================")
             print("0: Back")
-            print("1: Website version background management system: " + str(develop_web))
-            print("2: Special date celebration (will take up a small amount of resources): " + str(develop_celebrate))
-            print("3: Local visual control system (SSH): " + str(develop_CLI_SSH))
-            print("4: Local visual control system (SQL): " + str(develop_CLI_SQL))
+            print("1: Special date celebration (will take up a small amount of resources): " + str(develop_celebrate))
+            print("2: View the current theme version and the latest theme version on the homepage (requires theme support): " + str(develop_theme_version))
             develop_set_input = int(input())
             if develop_set_input == 0:
                 continue
-            elif develop_set_input == 1:
-                if webpy_import:
-                    conn_log = sqlite3.connect("log.db")
-                    cur_log = conn_log.cursor()
-                    cur_log.execute('''update DevelopSet set tf = 1 where id=1''')
-                    conn_log.commit()
-                    conn_log.close()
-                    os.system("clear")
-                    print("Develop Set SUCCESS")
-                    print("==========================================")
-                    print("The experimental feature is currently being used. This feature has not been fully tested and may cause unpredictable consequences.")
-                    time.sleep(3)
-                    continue
-                else:
-                    os.system("clear")
-                    print("+++++Error#9-PyBlog!+++++")
-                    print("+++++++Made By UTS+++++++")
-                    print("Github@underthestars-zhy/PyBlog")
-                    print("==========================================")
-                    print(">>> pip install web.py")
-                    sys.exit()
-            elif develop_set_input == 2:
+            elif develop_set_input == 1 and develop_celebrate == False:
+                conn_log = sqlite3.connect("log.db")
+                cur_log = conn_log.cursor()
+                cur_log.execute('''update DevelopSet set tf = 1 where id=1''')
+                conn_log.commit()
+                conn_log.close()
+                os.system("clear")
+                print("Develop Set SUCCESS")
+                print("==========================================")
+                print(
+                    "The experimental feature is currently being used. This feature has not been fully tested and may cause unpredictable consequences.")
+                time.sleep(3)
+                continue
+            elif develop_set_input == 2 and develop_theme_version == False:
                 conn_log = sqlite3.connect("log.db")
                 cur_log = conn_log.cursor()
                 cur_log.execute('''update DevelopSet set tf = 1 where id=2''')
                 conn_log.commit()
                 conn_log.close()
+                try:
+                    from theme_main import *
+
+                    os.system("rm version_theme.txt")
+                    os.system('wget ' + least_version_url())
+                    file = open('version_theme.txt', 'rt')
+                    version_theme = str(file.read())
+                    file.close()
+                    os.system("rm version_theme.txt")
+                    theme_version = "当前主题版本: " + version() + " 最新主题版本: " + version_theme
+                except:
+                    theme_version = "当前主题不支持/你还未安装主题"
                 os.system("clear")
                 print("Develop Set SUCCESS")
                 print("==========================================")
@@ -532,12 +633,13 @@ if __name__ == '__main__':
                     "The experimental feature is currently being used. This feature has not been fully tested and may cause unpredictable consequences.")
                 time.sleep(3)
                 continue
-            elif develop_set_input == 3:
+            elif develop_set_input == 1 and develop_celebrate == True:
                 conn_log = sqlite3.connect("log.db")
                 cur_log = conn_log.cursor()
-                cur_log.execute('''update DevelopSet set tf = 1 where id=3''')
+                cur_log.execute('''update DevelopSet set tf = 0 where id=1''')
                 conn_log.commit()
                 conn_log.close()
+                develop_web = False
                 os.system("clear")
                 print("Develop Set SUCCESS")
                 print("==========================================")
@@ -545,12 +647,13 @@ if __name__ == '__main__':
                     "The experimental feature is currently being used. This feature has not been fully tested and may cause unpredictable consequences.")
                 time.sleep(3)
                 continue
-            elif develop_set_input == 4:
+            elif develop_set_input == 2 and develop_theme_version == True:
                 conn_log = sqlite3.connect("log.db")
                 cur_log = conn_log.cursor()
-                cur_log.execute('''update DevelopSet set tf = 1 where id=4''')
+                cur_log.execute('''update DevelopSet set tf = 0 where id=2''')
                 conn_log.commit()
                 conn_log.close()
+                develop_celebrate = False
                 os.system("clear")
                 print("Develop Set SUCCESS")
                 print("==========================================")
@@ -558,6 +661,13 @@ if __name__ == '__main__':
                     "The experimental feature is currently being used. This feature has not been fully tested and may cause unpredictable consequences.")
                 time.sleep(3)
                 continue
+            else:
+                os.system("clear")
+                print("+++++Error#0-PyBlog!+++++")
+                print("+++++++Made By UTS+++++++")
+                print("Github@underthestars-zhy/PyBlog")
+                print("==========================================")
+                sys.exit()
         else:
             os.system("clear")
             print("+++++Error#0-PyBlog!+++++")
